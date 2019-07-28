a18ab3e04b7ccf2baa6f05db0c8790ff1711d1fd
Satvik Bisht <satvikbisht26@gmail.com>
2019-07-28 17:49:22 +0530

    first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,FormsModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+     cr1: any; x: number;
+     cr2: number;
+     cr3: number;
+     cr4: number;
+     cr5: number;
+     cr6: number;
+     cr7: number;
+     cr8: number;
+     cr9: number;
+     cr10: number;
+     cr11: number;
+     cr12: number;
   constructor() {}
 
-}
+    
+    
+   
+        per(){
+
+
+          var a = parseInt(this.cr1);
+        }
+     
+     
+
+
+
+      
+
+  }
+
+
+
+
+
+
+
+
